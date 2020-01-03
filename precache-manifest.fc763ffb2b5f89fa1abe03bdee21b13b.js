55d8990bbe1a76464c43fbe3208281b84fa199a7
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-03 12:04:31 +0600

    Updates

---
+++ precache-manifest.fc763ffb2b5f89fa1abe03bdee21b13b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b656d6591915ce161888033ff6c24968",
+    "url": "/card-dealer/index.html"
+  },
+  {
+    "revision": "458995971d869c5a8b24",
+    "url": "/card-dealer/static/css/main.8e4b3590.chunk.css"
+  },
+  {
+    "revision": "c2a0e6e87d87a06c6501",
+    "url": "/card-dealer/static/js/2.c3237161.chunk.js"
+  },
+  {
+    "revision": "458995971d869c5a8b24",
+    "url": "/card-dealer/static/js/main.510c3e80.chunk.js"
+  },
+  {
+    "revision": "cc0ad3e82c513ef7b746",
+    "url": "/card-dealer/static/js/runtime-main.401e0134.js"
+  }
+]);
